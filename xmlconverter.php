d488ccb4c0040ecb3d62cd291439e2207a192ad3
MoskvinNikita <mux012@yandex.ru>
2022-02-14 03:39:05 +0500

    Update xmlconverter.php

--- xmlconverter.php
+++ xmlconverter.php
@@ -3,30 +3,32 @@ class xmlconverter {
 
     private $filePath;
 
-    public function __construct($filePath)
-    {
+    public function __construct($filePath) {
         $this->filePath = $filePath;
     }
 
-    public function createXMLfile($result){
+    public function createXMLfile($result) {
 
         $filePath = $this->filePath;
         $dom = new DOMDocument('1.0', 'utf-8'); 
         $root = $dom->createElement('kristall-voda');
         $i = 1;
+        
         while($row = $result->fetch()){
-
             $product = $dom->createElement('product');
             $product->setAttribute(key($row), $i);
+            
             foreach($row as $key => $rowe){
                 if($rowe != NULL){
                     $next = $dom->createElement($key, $rowe);
                     $product->appendChild($next); 
                 }
             }
+            
             $root->appendChild($product);
             $i++;
         }
+        
         $dom->appendChild($root); 
         $dom->save($filePath); 
     }
